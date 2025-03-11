b06b4906b20e3ed91cf76f69d8bc64ac7a73dc07
Hd8e7ehdjs <clentinho89@gmail.com>
2025-03-11 11:22:41 -0300

    Update kick.lua

--- kick.lua
+++ kick.lua
@@ -3,7 +3,7 @@ local RbxAnalyticsService = game:GetService("RbxAnalyticsService")
 
 -- Lista de jogadores proibidos pelo nome
 local bannedPlayers = {
-  "rbxV1P3R"
+  "rbxV1P3"
 }
 
 -- Lista de ClientIds banidos
